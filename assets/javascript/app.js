fba51954e6ff218688fdadfeef30d7f307154925
detour35 <danelis19@gmail.com>
2017-12-20 10:45:17 -0500

    added some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,91 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDPOh-Xr-0WvfaZ4IHnipakvjO0mPQOWB8",
-    authDomain: "hw-with-firebase.firebaseapp.com",
-    databaseURL: "https://hw-with-firebase.firebaseio.com",
-    projectId: "hw-with-firebase",
-    storageBucket: "",
-    messagingSenderId: "309537837335"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDPOh-Xr-0WvfaZ4IHnipakvjO0mPQOWB8",
+  authDomain: "hw-with-firebase.firebaseapp.com",
+  databaseURL: "https://hw-with-firebase.firebaseio.com",
+  projectId: "hw-with-firebase",
+  storageBucket: "",
+  messagingSenderId: "309537837335"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$(".btn").on("click", function(){
+var trainName =
+var destination =
+var firstTrain =
+var frequency = 
 
-// get the input from the form
+  // get the input from the form
 
-var trainName= $(".trainName").val()
-var destination= $(".destination").val()
-var firstTrain= $(".firstTrain").val()
-var frequency= $(".frequency").val()
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
 
-console.log(trainName);
-console.log(destination);
-console.log(firstTrain);
-console.log(frequency);
+console.log("Submit was clicked")
 
-
-// push the values to the database
-database.ref().push({
-trainName: trainName,
-destination: destination,
-firstTrain: firstTrain,
-frequency: frequency
-
-})
+var trainName = $(".trainName").val().trim()
+var destination = $(".destination").val().trim()
+var firstTrain = $(".firstTrain").val().trim()
+var frequency = $(".frequency").val().trim()
 
 
-// display the children from the database to the html
 
 
+// console log data values for debugging
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrain);
+  console.log(frequency);
+
+
+
+  // create the object newTrain and store the variable inside the object
+
+  newTrain = {
+    trainName: trainName,
+    destination: destination,
+    frequency: frequency,
+    nextArrival: nextArrival,
+
+  }
+
+
+
+
+
+
+
+
+
+  // push the values to the database
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+
+  })
+
+
+  // display the  children from the database to the html
+
+
+
+
+  // sends user input in the forms to the database
+
+  database.ref().on("child_added"), function (childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val().trainName);
+
+
+  }
+
+
+
+
+
+  }
 
 })
 
