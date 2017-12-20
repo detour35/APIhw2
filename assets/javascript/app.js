53232a3ce2f89df854e17e05cf99e57765d97b51
detour35 <danelis19@gmail.com>
2017-12-19 21:05:10 -0500

    added some js variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDPOh-Xr-0WvfaZ4IHnipakvjO0mPQOWB8",
+    authDomain: "hw-with-firebase.firebaseapp.com",
+    databaseURL: "https://hw-with-firebase.firebaseio.com",
+    projectId: "hw-with-firebase",
+    storageBucket: "",
+    messagingSenderId: "309537837335"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
