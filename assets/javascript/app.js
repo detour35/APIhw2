9764906b1609347d468ca993e77fb1968a1c2d8e
detour35 <danelis19@gmail.com>
2017-12-21 11:07:04 -0500

    last commit to turn in hw

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,29 +9,28 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
+// create a variable to reference the database
 var database = firebase.database();
 
-var trainName =
-var destination =
-var firstTrain =
-var frequency = 
+// creates variables for our data
+var trainName;
+var destination;
+var firstTrain;
+var frequency;
 
-  // get the input from the form
+// Click function for the form
 
-  $(".btn").on("click", function (event) {
-    event.preventDefault();
+$(".btn").on("click", function (event) {
+  event.preventDefault();
 
-console.log("Submit was clicked")
+  console.log("Submit was clicked");
+  var trainName = $(".trainName").val().trim();
+  var destination = $(".destination").val().trim();
+  var firstTrain = $(".firstTrain").val().trim();
+  var frequency = $(".frequency").val().trim();
 
-var trainName = $(".trainName").val().trim()
-var destination = $(".destination").val().trim()
-var firstTrain = $(".firstTrain").val().trim()
-var frequency = $(".frequency").val().trim()
-
-
-
-
-// console log data values for debugging
+  // console log data values for debugging
   console.log(trainName);
   console.log(destination);
   console.log(firstTrain);
@@ -45,49 +44,34 @@ var frequency = $(".frequency").val().trim()
     trainName: trainName,
     destination: destination,
     frequency: frequency,
-    nextArrival: nextArrival,
-
-  }
-
-
-
-
-
-
-
-
-
-  // push the values to the database
-  database.ref().push({
-    trainName: trainName,
-    destination: destination,
     firstTrain: firstTrain,
-    frequency: frequency
-
-  })
-
-
-  // display the  children from the database to the html
-
-
-
-
-  // sends user input in the forms to the database
-
-  database.ref().on("child_added"), function (childSnapshot, prevChildKey) {
-
-    console.log(childSnapshot.val().trainName);
-
 
   }
 
-
-
-
-
-  }
+  // push the user values to the database
+  database.ref().push(newTrain);
+  trainName: $("trainName").val("");
+  destination: $("destination").val("");
+  firstTrain: $("firstTrain").val("");
+  frequency: $("frequency").val("");
 
 })
+
+// sends user input in the forms to the database
+
+database.ref().on("child_added", function (childSnap) {
+  // Appends new table data into the HTML
+  var newRow = $("<tr>");
+  newRow.append("<td>" + childSnap.val().trainName + "</td>");
+  newRow.append("<td>" + childSnap.val().destination + "</td>");
+  newRow.append("<td>" + childSnap.val().frequency + "</td>");
+  newRow.append("<td>" + childSnap.val().nextArrival + "</td>");
+  $("tbody").append(newRow);
+})
+
+
+
+
 
 
 
